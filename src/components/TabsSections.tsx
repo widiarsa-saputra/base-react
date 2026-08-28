0400a83501da63e9458be8fe8bc4ee9da01f3ad2
widiarsa-saputra <widiarsa.saputra.pro@gmail.com>
2026-08-28 11:47:38 +0800

    style: update border radius for TabsList and TabsTrigger components

--- src/components/TabsSections.tsx
+++ src/components/TabsSections.tsx
@@ -12,10 +12,10 @@ const TabsSections = (props: Props) => {
     return (
         <div className="flex flex-col h-full pt-4 px-4 pb-0 children-container relative">
             <Tabs defaultValue={props.tabObjects[0].trigger} className="w-full h-full flex flex-col">
-                <TabsList className="w-full bg-primary/10 relative">
+                <TabsList className="w-full bg-primary/10 relative rounded-md">
                     {
                         props.tabObjects.map(({ trigger }, idx) => (
-                            <TabsTrigger key={idx} value={trigger} className="font-semibold data-[state=active]:bg-primary data-[state=active]:text-primary-foreground">{trigger}</TabsTrigger>
+                            <TabsTrigger key={idx} value={trigger} className="font-semibold data-[state=active]:bg-primary data-[state=active]:text-primary-foreground !rounded">{trigger}</TabsTrigger>
                         ))
                     }
                 </TabsList>
