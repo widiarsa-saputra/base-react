90d0ee7ca3045a5c2c60bbb7b2e716009edf660a
widiarsa-saputra <widiarsa.saputra.pro@gmail.com>
2026-08-31 15:49:31 +0800

    feat: introduce useBaseUpdateSingleton hook and migrate GotraPaySetting to use it

--- src/services/GotraPaySetting/hooks/useSaveGotraPaySetting.ts
+++ src/services/GotraPaySetting/hooks/useSaveGotraPaySetting.ts
@@ -1,4 +1,4 @@
-import { useBaseUpdate } from "@/services/base/hooks/useBaseUpdate";
+import { useBaseUpdateSingleton } from "@/services/base/hooks/useBaseUpdateSingleton";
 import { GotraPaySettingPayload } from "../schema/GotraPaySettingSchema";
 import { GotraPaySettingResponseSchema, GotraPaySettingResponse } from "../response/GotraPaySettingResponse";
 
@@ -6,7 +6,7 @@ export const gotraPaySettingQueryKey = "gotrapay-setting";
 const API_VERSION = "v1";
 
 export const useSaveGotraPaySetting = () => {
-    return useBaseUpdate<GotraPaySettingPayload, GotraPaySettingResponse, { id: string | number }>({
+    return useBaseUpdateSingleton<GotraPaySettingPayload, GotraPaySettingResponse, GotraPaySettingPayload>({
         endpoint: () => `${API_VERSION}/gotrapay/setting`,
         schema: GotraPaySettingResponseSchema,
         queryKey: gotraPaySettingQueryKey,
